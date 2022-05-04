3df373e10512e8648eac86abb0e76fec84996106
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2022-05-04 11:24:55 +0700

    add theme share value

---
+++ lib/utils/theme.dart
@@ -0,0 +1,13 @@
+part of 'utils.dart';
+
+const double defaultMargin = 20;
+
+Color mainColor = Color(0xff00c853);
+Color secondaryColor = Color(0xff4caf50);
+Color fontLight = Colors.white70;
+Color fontDark = Colors.grey[700]!;
+Color fontAccent1 = Color(0xff00c853);
+
+TextStyle fontPrimary = GoogleFonts.poppins();
+TextStyle fontSecondary = GoogleFonts.nunito();
+TextStyle fontPacifico = GoogleFonts.pacifico();
