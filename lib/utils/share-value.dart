fe31d6607f03bedb0714e3dd2ba939b602ac8c2f
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2022-05-05 11:08:52 +0700

    add domain endpoint

--- lib/utils/share-value.dart
+++ lib/utils/share-value.dart
@@ -1,4 +1,5 @@
 part of 'utils.dart';
 
-int svSplashTime = 2000;
-String svDomain = "https://sigkdev.my.id/api/v1";
+int svSplashTime = 2500;
+String svApiDomain = "http://10.0.2.2:8000/api/pjsb-digital/v1/";
+String svAssetDomain = "http://10.0.2.2:8000/storage/thumbnail/";
