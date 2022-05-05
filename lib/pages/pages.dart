da488f5d991d6142af24242f55f298eb89298ab7
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2022-05-05 15:46:19 +0700

    add player page

--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -3,14 +3,17 @@ import 'dart:developer';
 
 import 'package:PJSBDigital/bloc/page_bloc.dart';
 import 'package:PJSBDigital/models/models.dart';
+import 'package:PJSBDigital/params/params.dart';
 import 'package:PJSBDigital/services/services.dart';
 import 'package:PJSBDigital/utils/utils.dart';
 import 'package:PJSBDigital/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 part 'splash-page.dart';
 part 'menu.dart';
 part 'levels.dart';
 part 'chapter-page.dart';
 part 'contents.dart';
+part 'player-page.dart';
